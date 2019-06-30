8e587408fc585725ba0d5dd90aa9201a9b079ab7
Lazyhacker <github@lazyhacker.com>
2019-06-30 14:09:10 -0700

    correct main package path and ignore .gcloudignore

--- server.go
+++ server.go
@@ -1,8 +1,10 @@
-package lazyhackergo
+package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +12,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<meta name=\"go-import\" content=\"%s%s git https://github.com/lazyhacker%s\">", r.Host, r.URL.Path, r.URL.Path)
 }
 
-func init() {
+func main() {
 	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
